7b6cf44b51d8a207470c9b20cac50fe9f44206f8
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-04-23 03:27:05 +0900

    formatter

---
+++ lambda/db.py
@@ -0,0 +1,12 @@
+import mysql.connector
+from datetime import date
+import os
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "root"),
+        database=os.getenv("DB_NAME", "gimme_scholarship"),
+    )
