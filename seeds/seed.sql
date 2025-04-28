3e4ee32cc0dfb344472c351b8b2c528d09367782
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-04-29 03:17:12 +0900

    院生の名前の変更，その他の追加

--- seeds/seed.sql
+++ seeds/seed.sql
@@ -1,2 +1,3 @@
 INSERT INTO `education_levels` (`id`, `name`) VALUES(1, '学部生');
-INSERT INTO `education_levels` (`id`, `name`) VALUES(2, '院生');
+INSERT INTO `education_levels` (`id`, `name`) VALUES(2, '大学院生');
+INSERT INTO `education_levels` (`id`, `name`) VALUES(3, 'その他');
