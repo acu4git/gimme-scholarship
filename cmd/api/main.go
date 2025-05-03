aeec970d0fa9ad26acf0dd1dfe445e52898549a2
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-04 01:08:46 +0900

    modified

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/acu4git/gimme-scholarship/internal/app/api/handler"
+	"github.com/acu4git/gimme-scholarship/internal/service"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	port = flag.Int("port", 3000, "port number")
+)
 
 func main() {
 	e := echo.New()
 	e.HideBanner = true
 	e.HidePort = true
+	e.Use(middleware.Recover())
+	e.Use(middleware.Secure())
+
+	repository, err := service.CreateRepository()
+	if err != nil {
+		log.Fatal(err)
+	}
+	handler := handler.NewAPIHandler(repository)
+
+	e.GET("/search", handler.SearchScholarships)
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", *port)))
 }
