feac0469c022ab5723387338bc32ef9f29e0e6e4
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-11 04:59:00 +0900

    DTO

---
+++ internal/app/api/handler/output.go
@@ -0,0 +1,41 @@
+package handler
+
+import "github.com/acu4git/gimme-scholarship/internal/domain/model"
+
+type scholarshipOutput struct {
+	ID             int64    `json:"id"`
+	Name           string   `json:"name"`
+	Address        string   `json:"address"`
+	Targets        []string `json:"targets"`
+	TargetDetail   string   `json:"target_detail"`
+	AmountDetail   string   `json:"amount_detail"`
+	TypeDetail     string   `json:"type_detail"`
+	CapacityDetail string   `json:"capacity_detail"`
+	DeadlineDetail string   `json:"deadline_detail"`
+	ContactPoint   string   `json:"contact_point"`
+	Remark         string   `json:"remark"`
+	PostingDate    string   `json:"posting_date"`
+}
+
+type GetScholarshipsOutput []scholarshipOutput
+
+func toGetScholarshipsOutput(scholarships []model.Scholarship) GetScholarshipsOutput {
+	res := make(GetScholarshipsOutput, 0)
+	for _, s := range scholarships {
+		res = append(res, scholarshipOutput{
+			ID:             s.ID,
+			Name:           s.Name,
+			Address:        s.Address,
+			Targets:        s.Targets,
+			TargetDetail:   s.TargetDetail,
+			AmountDetail:   s.AmountDetail,
+			TypeDetail:     s.TypeDetail,
+			CapacityDetail: s.CapacityDetail,
+			DeadlineDetail: s.DeadlineDetail,
+			ContactPoint:   s.ContactPoint,
+			Remark:         s.Remark,
+			PostingDate:    s.PostingDate,
+		})
+	}
+	return res
+}
