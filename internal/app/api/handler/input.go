f9262b3573e1fb197067117b2e76a6c86dd18879
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-18 04:49:35 +0900

    プロフィール登録の情報取得

--- internal/app/api/handler/input.go
+++ internal/app/api/handler/input.go
@@ -4,3 +4,10 @@ type GetScholarshipInput struct {
 	Target string `query:"target"`
 	Type   string `query:"type"`
 }
+
+type PostUserInput struct {
+	Email       string `json:"email"`
+	Grade       int    `json:"grade"`
+	Level       string `json:"level"`
+	AcceptEmail bool   `json:"accept_email"`
+}
