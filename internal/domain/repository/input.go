be4f6661b7bb9c8663fff135a6eab3df3ffd8672
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-20 00:45:32 +0900

    リポジトリへの入力の定義

--- internal/domain/repository/input.go
+++ internal/domain/repository/input.go
@@ -4,3 +4,11 @@ type FilterOption struct {
 	Target string
 	Type   string
 }
+
+type UserInput struct {
+	ID          string
+	Email       string
+	Level       string
+	Grade       int64
+	AcceptEmail bool
+}
