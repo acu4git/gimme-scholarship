31bbe63f1ca16dce0151d60778b380c2f8c50f00
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-28 04:10:14 +0900

    イメージ化

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	migrate "github.com/rubenv/sql-migrate"
+)
+
+var (
+	username = "root"
+	password = "root"
+	host     = "localhost"
+	port     = "3308"
+	database = "gimme_scholarship"
+)
+
+func main() {
+	applyMigration()
+}
+
+func applyMigration() {
+	if os.Getenv("DB_USERNAME") != "" {
+		username = os.Getenv("DB_USERNAME")
+	}
+	if os.Getenv("DB_PASSWORD") != "" {
+		password = os.Getenv("DB_PASSWORD")
+	}
+	if os.Getenv("DB_HOST") != "" {
+		host = os.Getenv("DB_HOST")
+	}
+	if os.Getenv("DB_PORT") != "" {
+		port = os.Getenv("DB_PORT")
+	}
+	if os.Getenv("DB_NAME") != "" {
+		database = os.Getenv("DB_NAME")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", username, password, host, port, database)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	migrations := &migrate.FileMigrationSource{
+		Dir: "migrations",
+	}
+
+	n, err := migrate.Exec(db, "mysql", migrations, migrate.Up)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("done %d migrations.\n", n)
 }
