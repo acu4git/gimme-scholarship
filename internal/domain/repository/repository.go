ff0cf099119ecb315cd4a3fab1ac9f6531974223
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-06-06 11:45:11 +0900

    お気に入り機能に関するメソッド

--- internal/domain/repository/repository.go
+++ internal/domain/repository/repository.go
@@ -5,4 +5,6 @@ import "github.com/acu4git/gimme-scholarship/internal/domain/model"
 type Repository interface {
 	CreateUser(UserInput) error
 	GetScholarships(FilterOption) ([]model.Scholarship, error)
+	RegisterFavoriteScholarship(UserFavoriteInput) error
+	DeleteFavoriteScholarship(UserFavoriteInput) error
 }
