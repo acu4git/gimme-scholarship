ac559209d24565cb84e21e57351337b000483db7
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-07-11 14:09:19 +0900

    テスト用のDB初期化

---
+++ internal/database/fake.go
@@ -0,0 +1,74 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/DATA-DOG/go-txdb"
+	"github.com/gocraft/dbr/v2"
+	"github.com/gocraft/dbr/v2/dialect"
+)
+
+func init() {
+	user, password, host, port, dbname := "root", "root", "127.0.0.1", "3308", "gimme_scholarship_test"
+	if os.Getenv("FAKE_DB_USER") != "" {
+		user = os.Getenv("FAKE_DB_USER")
+	}
+	if os.Getenv("FAKE_DB_PASSWORD") != "" {
+		user = os.Getenv("FAKE_DB_PASSWORD")
+	}
+	if os.Getenv("FAKE_DB_HOST") != "" {
+		user = os.Getenv("FAKE_DB_HOST")
+	}
+	if os.Getenv("FAKE_DB_PORT") != "" {
+		user = os.Getenv("FAKE_DB_PORT")
+	}
+	if os.Getenv("FAKE_DB_NAME") != "" {
+		user = os.Getenv("FAKE_DB_NAME")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		user,
+		password,
+		host,
+		port,
+		dbname,
+	)
+	txdb.Register("txdb", "mysql", dsn)
+}
+
+type FakeDatabase struct {
+	*Database
+}
+
+func NewFakeDatabase() *FakeDatabase {
+	fdb := &FakeDatabase{}
+	fdb.connect()
+	return fdb
+}
+
+func (f *FakeDatabase) connect() {
+	db, err := sql.Open("txdb", strconv.FormatInt(time.Now().UnixNano(), 10))
+	if err != nil {
+		panic(err)
+	}
+	conn := &dbr.Connection{
+		DB:            db,
+		EventReceiver: &dbr.NullEventReceiver{},
+		Dialect:       dialect.MySQL,
+	}
+	sess := conn.NewSession(nil)
+	if err = sess.Ping(); err != nil {
+		panic(err)
+	}
+	f.sess = sess
+}
+
+func (f *FakeDatabase) Close() {
+	if err := f.sess.Close(); err != nil {
+		panic(err)
+	}
+}
