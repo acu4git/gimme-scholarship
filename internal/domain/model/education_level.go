3ec81924f43ad114da408ee1846d68ef61805e54
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-07-16 01:31:00 +0900

    EducationLevelを文字列変換する関数

--- internal/domain/model/education_level.go
+++ internal/domain/model/education_level.go
@@ -8,6 +8,10 @@ const (
 	EducationLevelOther    EducationLevel = "その他"
 )
 
+func (m EducationLevel) Str() string {
+	return string(m)
+}
+
 func (m EducationLevel) Validate() bool {
 	if m != EducationLevelBacholor && m != EducationLevelMaster && m != EducationLevelOther {
 		return false
