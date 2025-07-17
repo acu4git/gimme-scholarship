da883cc1546512c99a99c74a9f683bc72ceec070
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-07-17 15:56:49 +0900

    MailKeyを用いたものに修正

--- internal/app/task/notify_scholarship_deadline.go
+++ internal/app/task/notify_scholarship_deadline.go
@@ -1,12 +1,10 @@
 package task
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"log"
 
-	"github.com/acu4git/gimme-scholarship/assets"
 	"github.com/acu4git/gimme-scholarship/internal/domain/model"
 	"github.com/acu4git/gimme-scholarship/internal/domain/repository"
 	"github.com/acu4git/gimme-scholarship/internal/service"
@@ -37,41 +35,16 @@ func (e *NotifyScholarshipDeadlineExecutor) Execute() error {
 		return nil
 	}
 
-	tmpl, err := assets.MailTemplatesTpl.Clone()
-	if err != nil {
-		return fmt.Errorf("failed to clone template: %w", err)
-	}
-
-	var subjectBuf bytes.Buffer
-	if err := tmpl.ExecuteTemplate(&subjectBuf, "deadline_notification_subject.txt", nil); err != nil {
-		return fmt.Errorf("failed to execute subject template: %w", err)
-	}
-
-	bodyTmpl := tmpl.Lookup("deadline_notification_body.txt")
-	if bodyTmpl == nil {
-		return fmt.Errorf("template 'deadline_notification_body.txt' not found")
-	}
-
-	var disclaimerBuf bytes.Buffer
-	if err := tmpl.ExecuteTemplate(&disclaimerBuf, "footer_disclaimer.txt", nil); err != nil {
-		return fmt.Errorf("failed to execute disclaimer template: %w", err)
-	}
-
-	var appInfoBuf bytes.Buffer
-	if err := tmpl.ExecuteTemplate(&appInfoBuf, "footer_app_info", nil); err != nil {
-		return fmt.Errorf("failed to execute app info template: %w", err)
-	}
-
 	bulkData := make([]service.BulkEmailData, 0, len(userScholarships))
 	for email, scholarships := range userScholarships {
-		templateData := struct {
-			Scholarships []model.Scholarship
-			Disclaimer   string
-			AppInfo      string
-		}{
-			Scholarships: scholarships,
-			Disclaimer:   disclaimerBuf.String(),
-			AppInfo:      appInfoBuf.String(),
+		text := ""
+		for i := range scholarships {
+			text += fmt.Sprintf("=== %d件目\n", i+1)
+			text += fmt.Sprintf("・奨学金名\n\t%s\n", scholarships[i].Name)
+			text += fmt.Sprintf("・形式\n\t%s\n\n", scholarships[i].TypeDetail)
+		}
+		templateData := map[string]any{
+			"Scholarships": text,
 		}
 		bulkData = append(bulkData, service.BulkEmailData{
 			To:           email,
@@ -81,7 +54,7 @@ func (e *NotifyScholarshipDeadlineExecutor) Execute() error {
 
 	ctx := context.Background()
 	log.Printf("Sending deadline notifications to %d users...\n", len(bulkData))
-	if err := e.mailer.SendBulkEmail(ctx, subjectBuf.String(), bodyTmpl, bulkData); err != nil {
+	if err := e.mailer.SendBulkEmail(ctx, model.MailKeyNotifyScholarshipDeadline, bulkData); err != nil {
 		return fmt.Errorf("failed to send bulk email: %w", err)
 	}
 
